ab9543eb9864e837b77b2b3bc5f944280f58c03f
Beni Kabona <beni.kabona@prypco.com>
2026-09-26 22:22:54 +0400

    [MO-4020] Wire Datadog Angular plugin and tracking options

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideDatadogErrorHandler, provideDatadogRouter } from '@datadog/browser-rum-angular';
 
 import { routes } from './app.routes';
 import { provideAmplitude } from '@core/amplitude';
@@ -16,6 +17,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
+    provideDatadogRouter(),
+    provideDatadogErrorHandler(),
     provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor, errorInterceptor])),
     provideAppConfig(),
     provideTheme(),
